33bfb8a28029e86d6a6b8d704f8a3b01343c2274
Kerdeygar <kerdey95@hotmail.com>
2022-11-24 23:30:57 -0600

    Se Agregan Archivos
    Se Agregan los archivos para conectar a firebase

---
+++ fire.js
@@ -0,0 +1,19 @@
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyDJALZqaNtPT7DmXu_ispUJLNYLcVQA1pE",
+  
+    authDomain: "chatonlyjs.firebaseapp.com",
+  
+    projectId: "chatonlyjs",
+  
+    storageBucket: "chatonlyjs.appspot.com",
+  
+    messagingSenderId: "475901697129",
+  
+    appId: "1:475901697129:web:46ff602d73b4941d9956f8"
+  
+  };
+  // Initialize Firebase
+  
+  firebase.initializeApp(firebaseConfig);
